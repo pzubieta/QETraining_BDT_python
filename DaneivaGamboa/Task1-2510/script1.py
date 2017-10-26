51f8b0adb6714ca54388dac01a15789e67b9ab15
Daneiva <daneiva@gmail.com>
2017-10-26 23:28:27 +0000

    task 1 arithmetic op

---
+++ DaneivaGamboa/Task1-2510/script1.py
@@ -0,0 +1,18 @@
+numeroMayor=100
+numeroMenor=10
+numeroMedio=50
+numeroExtra=9
+suma=numeroMedio+numeroMayor
+print("La suma es",suma)
+resta=numeroMayor-numeroMenor
+print("La resta es:",resta)
+multiplicacion=numeroMenor*numeroMedio
+print("La multiplicacion es:",multiplicacion)
+division=numeroMayor/numeroMenor
+print("La division es:",division)
+mod=numeroMenor%numeroMedio
+print("Mod es:", mod)
+exponent=numeroMenor**numeroMenor
+print("Resultado es", exponent)
+floatdiv=numeroMedio//numeroExtra
+print("Resultado de la division de",numeroMedio,"entre",numeroExtra,"es",floatdiv)
