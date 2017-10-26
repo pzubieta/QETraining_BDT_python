f0737a630c83b21d07241da452826193b5cbf9cf
diego <devsoftbol@gmail.com>
2017-10-26 23:11:43 +0000

    mi codigo

---
+++ DiegoGonzales/index.py
@@ -0,0 +1,17 @@
+num1 = 5
+num2 = 10
+suma = num1 + num2
+print (suma)
+print ("LA resta es: ", 10-5)
+print ("La multiplicacion", 100*2)
+print ("La division es: ", 100/2)
+print ("El modulo es:", 10%2)
+
+
+
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+
+
+
