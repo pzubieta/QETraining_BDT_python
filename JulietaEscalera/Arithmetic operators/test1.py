3c1f26b4e054a996be9a9a9ac82711ea8fea7281
Julieta <julieta.escalera@gmail.com>
2017-10-26 23:39:42 +0000

    arithmetic

---
+++ JulietaEscalera/Arithmetic operators/test1.py
@@ -0,0 +1,20 @@
+#Practice 1
+# Strings
+variable1_test= "first task"
+nombre= ("Julieta")
+edad=(2)
+print ("Hola "+ nombre)
+print(variable1_test)
+
+# Numbers
+number_int= 1000
+number_flo= 3.1416
+
+suma=number_int+number_flo
+resta=number_flo-number_int
+multipli= number_int*number_flo
+div=suma/3
+modulus1=suma%div
+exponet1=number_int**2
+
+print("suma=",suma,"resta=",resta,"multipli=", multipli,"div=",div,"modulus=",modulus1,"multipli=","exponent 2 =",exponet1)
