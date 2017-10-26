48d2dc03844d7b271b1493adeef080d2355ecb65
GladysCopaga <glacch7@gmail.com>
2017-10-26 23:43:16 +0000

    Submit commit about Task1 ArithmeticOperators in python

---
+++ GladysCopaga/Task1/ArithmeticOperators.py
@@ -0,0 +1,37 @@
+#declarando variables
+entero1 = 10
+entero2 = 20
+string = "Hello guys"
+prueba_tupla = string,entero2
+
+#Operacion Suma
+suma = entero1 + entero2
+print(suma)
+
+#Operacion Resta
+resta = entero1 - entero2
+print(resta)
+
+#Operacion Division
+division = entero1/entero2
+print(division)
+
+#Operacion Multiplicacion
+multiplicacion = entero1*entero2
+print("multiplicacion - resta:", multiplicacion - resta)
+
+#Operacion Modulo
+modulo = entero1%entero2
+print("modulo:",modulo)
+
+#Operacion Exponente
+exponente_prueba1 = entero1**entero2
+print(exponente_prueba1)
+
+#Ejemplo en clase de las tuplas
+
+
+#Imprmiendo cadenas
+print(string)
+print("suma:", suma)
+print ("concatenar:",string, prueba_tupla)
