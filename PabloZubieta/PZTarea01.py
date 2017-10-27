a4bdd4e588998d601a95533fa7f281271f2b9145
Pablo Zubieta <pablozub@gmail.com>
2017-10-27 19:29:56 -0400

    Adding first project by pz

--- PabloZubieta/PZTarea01.py
+++ PabloZubieta/PZTarea01.py
@@ -2,3 +2,7 @@ def print_lyrics():
     """ This function is to print lyrics and contains all code previously developed on class"""
     value_1 = 20
     value_2 = [20,9,0]
+     
+
+    if value_1 == value_2[0] :
+        result = value_2[1] + value_1
