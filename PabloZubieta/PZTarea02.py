c93b4289a94baa0224e695cde3a818861c6e70cb
Pablo Zubieta <pablozub@gmail.com>
2017-10-31 17:07:42 -0400

    Tarea2

--- PabloZubieta/PZTarea02.py
+++ PabloZubieta/PZTarea02.py
@@ -25,31 +25,32 @@ area_of_circle([15,3,7,12,17])
 sum_to (14)
 
 #------------------------------------------------------------------------------
-def evenOddCheck(numList)
+def evenOddCheck(numList):
     """Check is the given number is odd or even"""
     for i in numList:
-        if numList[i] % 2 = 0 : print("the number is EVEN."
+        if i % 2 == 0 : print("the number is EVEN.")
         else : print("the number is ODD.")
 
 
-def primesFrom(rangeNum)
+def primesFrom(rangeNum):
     count = 1
     while count < rangeNum:
         count = count + 1
-        if rangeNum > 35
+        if rangeNum > 35:
             print("the number is greater than 35: ", rangeNum)
             continue
 
 
+#------------------------------------------------------------------------------
 
-def daysInMonth(monthReq)
+def daysInMonth(monthReq):
     res = 31
-    if monthReq == "January" || monthReq == "March" || monthReq == "May" || monthReq == "July" || monthReq == "August" || monthReq == "October" || monthReq == "Dicember" :
-        print("The ", monthReq, " moth has: 31 days."
-    elif monthReq == "April" | | monthReq == "june" | | monthReq == "September" | | monthReq == "November":
-        print("The",  monthReq, " moth has: 30 days."
-    elif :
-        print("The ", monthReq, " moth has: 28 days."
+    if monthReq == "January" or monthReq == "March" or monthReq == "May" or monthReq == "July" or monthReq == "August" or monthReq == "October" or monthReq == "Dicember" :
+        print("The ", monthReq, " moth has: 31 days.")
+    elif monthReq == "April" or monthReq == "june" or monthReq == "September" or monthReq == "November":
+        print("The",  monthReq, " moth has: 30 days.")
+    else :
+        print("The ", monthReq, " moth has: 28 days.")
 
 numb = [9, 1, 3, 2]
 evenOddCheck(numb)
