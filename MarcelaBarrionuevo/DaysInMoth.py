62c9459838260e9b96736e37f93e7ae403de72dc
marceines <chelita141@gmail.com>
2017-10-31 07:17:18 -0400

    Days in the Month task
    Days in the Month task

--- MarcelaBarrionuevo/DaysInMoth.py
+++ MarcelaBarrionuevo/DaysInMoth.py
@@ -1,7 +1,6 @@
 def days_of_month(month):
-
     if month == "January":
-        print("Days in %s are: 31 " %  month)
+        print("Days in %s are: 31 " % month)
     elif month == "February":
         print("Days in %s are: 28 " % month)
     elif month == "March":
@@ -25,6 +24,51 @@ def days_of_month(month):
     elif month == "December":
         print("Days in %s are: 31 " % month)
     else:
-        print("Invalid operator")
+        print("Invalid Month")
+
 
 days_of_month("January")
+
+
+# Second solution
+
+def days_of_month_two(month):
+    if month == "January" or month == "March" or month == "May" or month == "July" or month == "August" or month == "September" or month == "October" or month == "December":
+        print("Days in %s are: 31 " % month)
+    elif month == "April" or month == "June" or month == "November":
+        print("Days in %s are: 30 " % month)
+    elif month == "February":
+        print("Days in %s are: 28 " % month)
+    else:
+        print("Invalid Month")
+
+
+days_of_month_two("February")
+
+# third solution
+
+def days_of_month_three(month):
+    dayswiththirtyone = ['January','March', 'May', 'July', 'August', 'September', 'October', 'December']
+    dayswiththirty = ['April', 'June', 'November']
+    dayfebruary = "February"
+
+    foundmonth = False
+    for val in dayswiththirtyone:
+        if val == month:
+            print("Days in %s are: 31 " % month)
+            foundmonth = True
+
+    if foundmonth == False:
+        for valtwo in dayswiththirty:
+            if valtwo == month:
+                print("Days in %s are: 30 " % month)
+                foundmonth = True
+
+    if dayfebruary == month:
+        print("Days in %s are: 28 " % month)
+        foundmonth == True
+
+    if foundmonth == False:
+        print("Invalid Month")
+
+days_of_month_three("June")
