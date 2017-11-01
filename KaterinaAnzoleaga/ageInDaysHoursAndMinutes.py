7bbd97c5cfc8f8a3f23b639737bd58031bb16ccd
kanzoleaga <katerina.anzoleaga@gmail.com>
2017-11-01 19:35:17 -0400

    Modules homework

---
+++ KaterinaAnzoleaga/ageInDaysHoursAndMinutes.py
@@ -0,0 +1,11 @@
+# Calculate age in minutes hours and days
+
+def ageInMinutes (age):
+    return age*365*24*60
+
+def ageInHours (age):
+    return age*365*24
+
+def ageInDays (age):
+    return age*365
+
