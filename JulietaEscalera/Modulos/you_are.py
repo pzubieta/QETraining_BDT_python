5ac1a16649d0e480a24ed1e634d1a52c695faba7
Julieta <julieta.escalera@gmail.com>
2017-11-01 23:52:15 +0000

    module_class

---
+++ JulietaEscalera/Modulos/you_are.py
@@ -0,0 +1,14 @@
+def you_are(edad):
+    if edad<=12:
+        print("Tu eres un niño ")
+    elif(edad>12 and edad<=17):
+        print("Tu eres un adolescente ")
+    elif(edad>18 and edad<=29):
+        print("Tu eres un Joven ")
+    elif(edad>=30 and edad<=100):
+        print("Tu eres adulto")
+    else: print("Tu eres una persona de mas de 100 años")
+
+
+
+
