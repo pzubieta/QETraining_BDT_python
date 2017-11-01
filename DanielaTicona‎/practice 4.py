bf3396b29ce2ac97dc8489d181dd794932e989ab
dxnis <daniela.ticona@gmail.com>
2017-11-01 08:01:30 -0400

    Practice 4 (String handling, string and list, dictionary)

--- DanielaTicona‎/practice 4.py
+++ DanielaTicona‎/practice 4.py
@@ -46,5 +46,20 @@ print "su reemplazo es:", phrase
 
 print "+_+_+_+_+_+_+_+_+_+_+_+_+  Exercise 3   +_+_+_+_+_+_+_+_+_+_+_+_"
 
+alpha ={'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0,' ':0}
+r="A mi me GustA LA SAlchipApaAA"
+y= r.lower()
+print y
+for i in range (len(y)):
+    if alpha.has_key(y[i]):
+        word=y[i]
+        alpha[word]= alpha[word]+1
+        #print "esta el valor", word, "y este es el valor", alpha[word]
+    else:
+        print "nuu hay"
+print "the String is:", y
+print alpha
+#print alpha.keys(), alpha.values()
+
 
 
