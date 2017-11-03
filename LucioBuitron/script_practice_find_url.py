e8f9570925da6b2013c9c8a4a1b241d9b3a1a877
luciobuitron <luciobuitron@gmail.com>
2017-11-03 19:38:35 -0400

    Script practice #3

--- LucioBuitron/script_practice_find_url.py
+++ LucioBuitron/script_practice_find_url.py
@@ -3,7 +3,7 @@ text1 = "I am in https://google.com page in my browser"
 text2 = "I am in google.com page in my browser"
 text3 = "Yesterday I was browsing in many pages, like example the URL http://www.hotmail.es "
 
-#FUNCION QUE IDENTIFICA 1 URL DE TIPO .COM
+#FUNCION #1 QUE IDENTIFICA 1 URL DE TIPO .COM
 def find_url(text):
     position1=text.find("http")
     position2 = text.find(".com")
@@ -16,7 +16,7 @@ def find_url(text):
         print("No existe una URL en el texto.")
 
 
-#FUNCION QUE IDENTIFICA 1 URL DE CUALQUIER EXTENSION
+#FUNCION #2 QUE IDENTIFICA 1 URL DE CUALQUIER EXTENSION
 def find_url2(text):
     position1=text.find("http")
     partial_url = text[position1:]
