52e9cb6aa1f4a817856a14d043b9daf06f206cb1
mauricioZelaya <zelaya.mauricio@gmail.com>
2017-11-03 14:55:06 -0400

    adding modules and script to calculate area and perimeter of a circle

---
+++ MauricioZelaya/circleOperations/circleScript.py
@@ -0,0 +1,7 @@
+from areaCalculator import areaCalculator
+from perimeterCalculator import perimeterCalculator
+
+
+circleRadius = 3
+print (areaCalculator(circleRadius))
+print(perimeterCalculator(circleRadius))
