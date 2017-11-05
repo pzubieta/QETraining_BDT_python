233840051f695bac2180a2617e7c299d371ea904
pzubieta <pablozub@gmail.com>
2017-11-05 18:06:52 -0400

    Adding more practices and Tasks

--- PabloZubieta/PZProy11_LetterCounterTable.py
+++ PabloZubieta/PZProy11_LetterCounterTable.py
@@ -1,13 +1,33 @@
-def lettersTable(strWord, char):
-    count = 0
-    while count <= len(strWord):
-        for char in word:
-            if char == strWord[count]:
-                count += 1
-            return count
+def lettersTable(strWord):
+    letterTable = {}
+    for i in range(97,123) :
+        letterTable[chr(i)] = count_letters1(strWord, chr(i))
 
-def count_letters(word, char):
-  return sum(char == c for c in word)
+    return(letterTable)
 
-print lettersTable('banana','a')
+def count_letters1(strWord, charEval):
+
+    letterCount = 0
+
+    # Transforma todo a minusculas
+    strWord = str.lower(strWord)
+
+
+    for char in strWord:
+        if charEval == char:
+            letterCount += 1
+
+    return letterCount
+
+def count_letters2(word, char):
+    """"very efficient way to count letters into a string"""
+    return sum(char == c for c in word)
+
+print (count_letters1('AAAAAbanana','a'))
+
+print (count_letters2('aaabanana','a'))
+
+strRes = lettersTable('AAAaabananazzz' )
+for chrLetter in strRes:
+    print(chrLetter[0], " : ", strRes[chrLetter], "\n")
 
