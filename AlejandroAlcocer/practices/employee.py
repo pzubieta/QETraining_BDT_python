396bd90a4abdac46e8862be2b78b4ce50d9df2da
Sl3v7n <slevin.v77gmail.com>
2017-11-06 17:26:18 -0400

    Modifying person and employee class with the proper constructors and adding methods to get

--- AlejandroAlcocer/practices/employee.py
+++ AlejandroAlcocer/practices/employee.py
@@ -1,9 +1,20 @@
-import AlejandroAlcocer.practices.person as person
+from AlejandroAlcocer.practices.person import Person
 
-class Employee(person):
+class Employee(Person):
+
 
     def __init__(self, name, last_name, age, ci, employee_id, department):
-        person.Person.__init__(self, name, last_name, age, ci)
+        Person.__init__(self, name, last_name, age, ci)
         self.employee_id = employee_id
         self.department = department
 
+    def get_employee(self):
+        employee = (Person.get_person(self))
+        return employee.extend([self.employee_id, self.department])
+
+
+####################  TEST    ####################
+
+e = Employee("name", "las_name", 30, 45, 123, "dev")
+print(e.get_person())
+print(e.get_employee())
