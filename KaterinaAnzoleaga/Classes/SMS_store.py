6f481cb7dadff58a100e9208f15ef8874420356f
Katerina Anzoleaga Murillo <Katerina Anzoleaga@jalasoft.local>
2017-11-06 15:38:01 -0400

    Some improvements to SMS Class

--- KaterinaAnzoleaga/Classes/SMS_store.py
+++ KaterinaAnzoleaga/Classes/SMS_store.py
@@ -1,3 +1,5 @@
+import datetime
+
 class SMS_store:
     """This class allows to simulate SMS inbox actions"""
 
@@ -5,33 +7,42 @@ class SMS_store:
         self.inbox = []
 
     def add_new_arrival(self, from_number, time_arrived, text_of_SMS):
-        dic = {'read': False, 'from_number': from_number, 'time_arrived': time_arrived, 'text_of_SMS': text_of_SMS}
-        self.inbox.append(dic)
+        if int(from_number) and isinstance(time_arrived, datetime.datetime):
+            dic = {'read': False, 'from_number': from_number, 'time_arrived': time_arrived, 'text_of_SMS': text_of_SMS}
+            self.inbox.append(dic)
+        else:
+            print ("Invalid input!")
 
     def message_count(self):
-        return (self.inbox.len)
+        return (len(self.inbox))
 
     def get_unread_indexces(self):
         not_read = []
-        for i in self.inbox:
-            if i['read'] == False: not_read.append(i)
+        for i in range (len(self.inbox)):
+            if self.inbox[i]['read'] == False: not_read.append(i)
         return not_read
 
     def get_message(self, i):
         if i in range(len(self.inbox)):
             self.inbox[i]['read'] = True
-            return (self.inbox[i])
+            return (self.inbox[i]['from_number'],str(self.inbox[i]['time_arrived'].strftime("%y-%m-%d %H:%M")),self.inbox[i]['text_of_SMS'])
         else:
             return (None)
-
+    def print_raw_index (self):
+        for i in my_inbox.inbox:
+            print(i)
 
 my_inbox = SMS_store()
-my_inbox.add_new_arrival('457554', '12:30', 'Uno')
-my_inbox.add_new_arrival('7857554', '12:30', 'Dos')
-my_inbox.add_new_arrival('777554', '12:30', 'Tres')
-my_inbox.add_new_arrival('687554', '12:30', 'Cuatro')
-my_inbox.add_new_arrival('7014554', '12:30', 'Cinco')
-print(my_inbox.get_message(2))
-print(my_inbox.get_unread_indexces())
-print(my_inbox.get_message(3))
-print(my_inbox.get_unread_indexces())
+
+my_inbox.add_new_arrival('457554', datetime.datetime.now(), 'Uno')
+my_inbox.add_new_arrival('7857554',datetime.datetime.now(), 'Dos')
+my_inbox.add_new_arrival('777554', datetime.datetime.now(), 'Tres')
+my_inbox.add_new_arrival('687554', datetime.datetime.now(), 'Cuatro')
+my_inbox.add_new_arrival('7014554', datetime.datetime.now(), 'Cinco')
+my_inbox.print_raw_index()
+print ("Messages in the inbox: ", my_inbox.get_unread_indexces() )
+print ("Number or messages in the inbox: ", my_inbox.message_count())
+print("Getting messges from index [2]", my_inbox.get_message(2))
+print("Messages not read: ", my_inbox.get_unread_indexces())
+print("Getting messges from index [3]", my_inbox.get_message(3))
+print("Messages not read: ", my_inbox.get_unread_indexces())
