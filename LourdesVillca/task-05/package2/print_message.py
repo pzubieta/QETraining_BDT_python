4d0dc384321a04da5161cd1c39672cdf18c16c43
lourdesVillca <lourdes.villca@fundacion-jala.org>
2017-11-05 22:08:09 -0400

    document implemented methods

--- LourdesVillca/task-05/package2/print_message.py
+++ LourdesVillca/task-05/package2/print_message.py
@@ -1,9 +1,14 @@
 def print_age_message(age):
+    """
+    Method to print message according the range of the age.
+
+    :param age:     int     Represents the age value to validate.
+    """
     if age <= 12:
         print("You are a child")
-    elif age >= 13 and age <= 17:
+    elif age <= 13 <= 17:
         print("You are a teenager ")
-    elif age >= 18 and age <= 29:
+    elif age <= 18 <= 29:
         print("You are young ")
     else:
         print("You are adult")
