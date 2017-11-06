941302c3a800f34591157371d7003ed5b1713584
lourdesVillca <lourdes.villca@fundacion-jala.org>
2017-11-05 21:59:39 -0400

    Add task related to strings, lists, dictionary and modules

---
+++ LourdesVillca/task-05/package1/calculate_age.py
@@ -0,0 +1,10 @@
+def calculate_age_minutes(age):
+    return age * 365 * 24 * 60
+
+
+def calculate_age_hours(age):
+    return age * 365 * 24
+
+
+def calculate_age_days(age):
+    return age * 365
---
+++ LourdesVillca/task-05/package2/print_message.py
@@ -0,0 +1,9 @@
+def print_age_message(age):
+    if age <= 12:
+        print("You are a child")
+    elif age >= 13 and age <= 17:
+        print("You are a teenager ")
+    elif age >= 18 and age <= 29:
+        print("You are young ")
+    else:
+        print("You are adult")
