6cf574c6885f33590fa046eaaf26aca09019ffce
RosarioFalconi <bruxita2312@gmail.com>
2017-11-06 23:00:49 -0400

    Practice about modules

---
+++ RosarioFalconi/Practica_4/Task4.py
@@ -0,0 +1,15 @@
+import Task4_Msg as msg
+import Task4_Calculate_Age as ca
+
+def calling_module_functions (edad):
+    if not edad.isnumeric():
+        print("Edad no valida")
+    else:
+        age2 = int(edad)
+        msg.age_message(age2)
+        ca.age_days(age2)
+        ca.age_hours(age2)
+        ca.age_minutes(age2)
+
+#age = input ("Ingrese la edad:")
+#calling_module_functions(age)
