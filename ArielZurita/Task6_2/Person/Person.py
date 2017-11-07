d6b4b59e1b5d9f1bbec6f7d2813c41dad3a007ab
Ariel Zurita Merida <Ariel Zurita@jalasoft.local>
2017-11-07 17:33:04 -0400

    Updating task 5 and task 6 related to classes and functions

---
+++ ArielZurita/Task6_2/Person/Person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, firstName, lastName, age, cI):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.cI = cI
