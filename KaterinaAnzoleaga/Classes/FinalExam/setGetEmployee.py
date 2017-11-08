e824514297f3c58a5799c338f4e16127b31dd748
kanzoleaga <katerina.anzoleaga@gmail.com>
2017-11-07 20:17:30 -0400

    Almost done loggin missing

--- KaterinaAnzoleaga/Classes/FinalExam/setGetEmployee.py
+++ KaterinaAnzoleaga/Classes/FinalExam/setGetEmployee.py
@@ -10,16 +10,24 @@ print (anEmplo.getObject())
 
 number = int (input("Ingrese el numero de empleados: "))
 
+list = []
 for i in range (number):
     name = input ("Name: ")
     last = input("Lastname: ")
     deparment = input("Department: ")
-    anEmplo = Employee (name, last, deparment)
+    anEmplo = Employee name, last, deparment)
     if deparment == 'Factory':
-        effective = input('Effective pieces: ')
+        effective = int (input('Effective pieces: '))
         anEmplo.setEfectivePieces (effective)
-        defective = input('Defective pieces: ')
-        anEmplo.setdefective_pieces(defective)
+        defective = int (input('Defective pieces: '))
+        anEmplo.setDefectivePieces(defective)
     elif deparment == 'Sales':
-        sold = input('Pieces sold: ')
+        sold = int (input('Pieces sold: '))
         anEmplo.setPiecesSold (sold)
+    anEmplo.calcSalary ()
+    anEmplo.calcTotalDiscount()
+    anEmplo.calcNetSalary ()
+    list.append(anEmplo.getObject())
+
+for i in list:
+    print(i)
