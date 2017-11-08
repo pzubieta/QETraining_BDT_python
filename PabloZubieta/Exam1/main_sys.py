2025ed40ab9209ab8fa54559ae0a765b14909d62
Pablo Zubieta <pablozub@gmail.com>
2017-11-07 21:31:47 -0400

    Last exam Fixes

--- PabloZubieta/Exam1/main_sys.py
+++ PabloZubieta/Exam1/main_sys.py
@@ -10,7 +10,8 @@ count = 1
 saleflag = 0
 f_employeelist = []
 s_employeelist = []
-logging.info('Filling the employees')
+logging.info('Filling1 the employees')
+
 while count <= nroemp:
     count = count + 1
     first = input("First name: ")
@@ -18,14 +19,15 @@ while count <= nroemp:
     dep = input("Department: ")
     glo = input("Global Salary: ")
 
-    if int(input("Is it a sales employee? 1->yes 0->no: ")):
+    if int(input("Is it a sales employee? 1->yes 0->no: ")) == 1:
         psold = int(input("Num of Pieces sold: "))
         saleflag = 1
     else:
         peff = int(input("Num of Effective Pieces: "))
         pdef = int(input("Num of Defective Pieces: "))
+        saleflag = 0
 
-    if saleflag:
+    if saleflag == 1:
         employee = Sales_Employee(first,last,dep,glo,psold,12.5)
         s_employeelist.append(employee)
     else:
@@ -33,17 +35,27 @@ while count <= nroemp:
         f_employeelist.append(employee)
     print("---------Enter next employee data-----")
     logging.info('Employee has been filled out, asking for next employee')
-
+'''
+# testing data
 logging.info('Calculating salaries and printing table')
-print ("Name     !        Departament     !  Global Sal  ! Total Disc  ! Net Salary!")
-for employee in s_employeelist:
-    glosal = employee.getglobalsalary()
-    totsal = salary_calc.total_salary_calc_sales(employee)
-    netsal = salary_calc.net_salary_calc_sales(employee)
-    print(employee.getname(),"   !   ", employee.getdepartament(),"   !   ",glosal,"   !   ",totsal,"   !   ",netsal)
+employee1 = Factory_Employee("Pablo", "Zubieta", "Dep1", 100,12.5, 10, 1)
+employee2 = Factory_Employee("Pablo2", "Zubieta2", "Dep2", 200,12.5, 20, 2)
+employee3 = Sales_Employee("Pablo3", "Zubieta3", "Dep3", 300, 30, 12.5)
 
-for employee in f_employeelist:
-    glosal = employee.getglobalsalary()
-    totsal = salary_calc.total_salary_calc_factory(employee)
-    netsal = salary_calc.net_salary_calc_factory(employee)
-    print(employee.getname(),"   !   ", employee.getdepartament(),"   !   ",glosal,"   !   ",totsal,"   !   ",netsal)
+f_employeelist.append(employee1)
+f_employeelist.append(employee2)
+s_employeelist.append(employee3)
+'''
+
+print ("Name     !        Departament     !  Global Sal  ! Total Disc  ! Net Salary!")
+for employee_s in s_employeelist:
+    glosal = employee_s.getglobalsalary()
+    totsal = salary_calc.total_salary_calc_sales(employee_s)
+    netsal = salary_calc.net_salary_calc_sales(employee_s)
+    print(employee_s.getname(),"   !   ", employee_s.getdepartament(),"   !   ",glosal,"   !   ",totsal,"   !   ",netsal)
+
+for employee_f in f_employeelist:
+    glosal = employee_f.getglobalsalary()
+    totsal = salary_calc.total_salary_calc_factory(employee_f)
+    netsal = salary_calc.net_salary_calc_factory(employee_f)
+    print(employee_f.getname(),"   !   ", employee_f.getdepartament(),"   !   ",glosal,"   !   ",totsal,"   !   ",netsal)
--- PabloZubieta/Exam1/salary_calc.py
+++ PabloZubieta/Exam1/salary_calc.py
@@ -3,11 +3,11 @@ def total_salary_calc_sales(employee):
     return (employee.getpiecessold()*2.5)
 
 def total_salary_calc_factory(employee):
-    return (employee.getpiecessold()*10 - employee.getdefpieces()*1.3)
+    return (employee.geteffpieces()*10 - employee.getdefpieces()*1.3)
 
 def net_salary_calc_factory(employee):
     totalsal = total_salary_calc_factory(employee)
-    disc = employee.getdiscount()
+    disc = employee.gettotaldiscount()
     netsal = totalsal-(totalsal*disc/100)
     return netsal
 
