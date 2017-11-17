5dd300445114cbfc16db1f2f486f8ef0b39f4545
Pablo Zubieta <pablozub@gmail.com>
2017-11-17 18:41:14 -0400

    test

--- PabloZubieta/Behave/APItest/ATM_Feature/environment.py
+++ PabloZubieta/Behave/APItest/ATM_Feature/environment.py
@@ -1,9 +1,14 @@
+
+
 def before_all(context):
+#The command to run behave skipping scenarios (test cases) with certain tags is:
+#>  behave ATM_feature2Outline.feature -f plain --tags @pz --summary --no-capture
+
     print("******************BEFORE ALL******************")
 
 def before_feature(context, feature):
-    if 'try' in feature.tags:
-        print("******************FEATURE TRY TAG******************")
+    if 'tag_feature' in feature.tags:
+        print("******************FEATURE tag_feature TAG******************")
     if 'Test' in feature.name:
         print("******************FEATURE TRY Test******************")
 def before_scenario(context, scenario):
@@ -13,7 +18,7 @@ def before_scenario(context, scenario):
         print("_____________BEFORE scenario________________")
 
 def before_step(context, step):
-    if 'tag_step' in step.tags:
-        print("*------------BEFORE step tag-----------------*")
-    if 'Test' in step.tags:
+    if 'withdraw' in step.name:
+        print("*------------BEFORE step withdraw-----------------*")
+    if 'Test' in step.name:
         print("*------------BEFORE step test-----------------*")
