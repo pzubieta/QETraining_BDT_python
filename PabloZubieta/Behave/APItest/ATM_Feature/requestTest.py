e263dc9941f02ec6f574b12e1b17d03e2a3a72a7
Pablo Zubieta <pablozub@gmail.com>
2017-11-17 19:52:42 -0400

    test

--- PabloZubieta/Behave/APItest/ATM_Feature/requestTest.py
+++ PabloZubieta/Behave/APItest/ATM_Feature/requestTest.py
@@ -5,3 +5,12 @@ r = requerst.get('http://apigithub.com/events')
 print("Values \n", r.text)
 print("CONTENT \n", r.content)
 print("CODE \n", r.status_code)
+print("JSON \n", r.json)
+
+payload = {'key1':'value1', 'key2':'value2'}
+
+r = requests.post("http://httpbin.org/post", data = payload)
+print(r.text)
+
+url = 'http://apigithub.com/event'
+headers = {'user'}
