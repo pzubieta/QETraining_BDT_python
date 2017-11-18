110e9c83dcdb66ea2aaf48ace63352cc733b93a6
r.alejandroalcocervega <slevin.v77@gmail.com>
2017-11-18 18:08:24 -0400

    Adding some fixes for get

--- MauricioZelaya/BDT-YTAPI/utils/utils.py
+++ MauricioZelaya/BDT-YTAPI/utils/utils.py
@@ -1,7 +1,16 @@
 import requests
 
-
-def getConnection(host, rootPath):
-    endpoint = host+rootPath+"/comments"
-    r = requests.get(endpoint)
-    return r.status_code
+def get_conn(host, root_path, method, payload = None):
+    """
+    this method will request a get and return the status code
+    :param host: the host
+    :param root_path: the rootpath of the url
+    :param method: the method that will me test
+    :param key: API key of the user
+    :param snippet: optional param for some methods
+    :return: the status code
+    """
+    endpoint = host + root_path + method
+    payload = payload
+    request = requests.get(endpoint, payload)
+    return request.status_code
